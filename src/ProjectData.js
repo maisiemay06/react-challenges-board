84e15122dc0144f4dd0bc2841d18fabbd686a49b
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-04-27 16:46:56 +0100

    update accordion and menu data

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,92 +1,92 @@
 export default [
-    {
-        id: 1,
-        title: 'Birthday Reminder',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 2,
-        title: 'Tours',
-        image: require('./images/tours.png'),
-        link: 'https://dazzling-buttercream-934333.netlify.app/'
-    },
-    {
-        id: 3,
-        title: 'Reviews',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 4,
-        title: 'Accordion',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 5,
-        title: 'Menu',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 6,
-        title: 'Tabs',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 7,
-        title: 'Slider',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 8,
-        title: 'Lorem Ipsum',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 9,
-        title: 'Color Generator',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 10,
-        title: 'Grocery Bud',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 11,
-        title: 'Navbar',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 12,
-        title: 'Sidebar And Modal',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 13,
-        title: 'Stripe Menu',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 14,
-        title: 'Cart',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    },
-    {
-        id: 15,
-        title: 'Cocktails',
-        image: require('./images/birthdayreminder.png'),
-        link: 'https://soft-melba-d5d8d3.netlify.app/'
-    }
-]
+  {
+    id: 1,
+    title: "Birthday Reminder",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Tours",
+    image: require("./images/tours.png"),
+    link: "https://dazzling-buttercream-934333.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Reviews",
+    image: require("./images/reviews.png"),
+    link: "https://roaring-dragon-dcbb66.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Accordion",
+    image: require("./images/accordion.png"),
+    link: "https://vermillion-cascaron-6e1892.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Menu",
+    image: require("./images/menu.png"),
+    link: "https://lucky-pegasus-a8e32d.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Tabs",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Slider",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Lorem Ipsum",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Color Generator",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "Grocery Bud",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 11,
+    title: "Navbar",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 12,
+    title: "Sidebar And Modal",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 13,
+    title: "Stripe Menu",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 14,
+    title: "Cart",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+  {
+    id: 15,
+    title: "Cocktails",
+    image: require("./images/birthdayreminder.png"),
+    link: "https://soft-melba-d5d8d3.netlify.app/",
+  },
+];
